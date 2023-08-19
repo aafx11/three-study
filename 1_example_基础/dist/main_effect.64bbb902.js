d6ee272c7f9f85f167da8167fb7d247fa0d71308
fx <903921845@qq.com>
2023-08-19 10:14:20 +0800

    css2d

--- 1_example_基础/dist/main_effect.64bbb902.js
+++ 1_example_基础/dist/main_effect.64bbb902.js
@@ -45940,7 +45940,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1221" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4735" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
